7b103808918cbe2653a7fcb259a69cb3997e94e7
alejandro11magallanes <20170023@uttcampus.edu.mx>
2024-05-03 09:57:24 -0600

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import { Box, Button, Typography } from '@mui/material';
+import { DataGrid } from '@mui/x-data-grid';
+import { DatosEmpleados } from './datos';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [Ocultar, setOcultar] = useState(false);
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 100 },
+    { field: 'nombre', headerName: 'Nombre', width: 200 }, 
+  ];
+
+  const handleGenerarClick = () => {
+    setOcultar(true); // Mostrar el DataGrid y el botón "Limpiar"
+  };
+
+  const handleLimpiarClick = () => {
+    setOcultar(false); // Ocultar el DataGrid y el botón "Limpiar"
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+    <Box sx={{display: 'flex', justifyContent: 'center', width: "100%", backgroundColor: "#2644cd", height: "80px"}}>
+      <Typography variant='h4' sx={{color: "#fff", marginTop: "20px"}}>Sistema de Cotizaciones</Typography>
+    </Box> 
+    <Box m={10}>
+      <Box sx={{display: "flex"}}>
+      <Typography>Solicitar Catalogo de Empleados</Typography>
+      <Button sx={{marginLeft: "70%", }}  variant="contained" onClick={handleGenerarClick}>Generar</Button> 
+      </Box>
+      <br/>
+      {Ocultar && ( // Mostrar el DataGrid y el botón "Limpiar" solo si Ocultar es verdadero
+          <>
+            <DataGrid
+              rows={DatosEmpleados}
+              columns={columns}
+              initialState={{
+                pagination: {
+                  paginationModel: {
+                    pageSize: 5,
+                  },
+                },
+              }}
+              pageSizeOptions={[10]}
+              disableRowSelectionOnClick
+            />
+            <Box sx={{ display: 'flex' }}>
+              <Button sx={{ marginLeft: '93%' }} variant="contained" onClick={handleLimpiarClick}>Limpiar</Button>
+            </Box>
+          </>
+        )}
+    </Box>
     </div>
   );
 }
---
+++ src/datos.js
@@ -0,0 +1,29 @@
+export const DatosEmpleados = [
+    {
+        id: 1,
+        nombre: "Alejandro Pineda",
+    },
+    {
+        id: 2,
+        nombre: "Luis Aguirre",
+    },
+    {
+        id: 3,
+        nombre: "Jorge Esparza",
+    },
+    {
+        id: 4,
+        nombre: "Nailea Vidrio",
+    },
+    {
+        id: 5,
+        nombre: "Alondra Torres",
+    },
+    {
+        id: 6,
+        nombre: "Jesus Lozano",
+    },{
+        id: 7,
+        nombre: "Marta Robles",
+    }
+]
